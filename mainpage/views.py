3676b16e3f1a26727f4885355277d6e432436c5c
marcosan <marcoxavibsc@gmail.com>
2018-01-31 10:18:32 -0500

    [NEW] creacion del servidor

---
+++ mainpage/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'index.html', {'posts': posts})
