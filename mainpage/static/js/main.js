eaf490a8c3a99c5d458e0f2a6f140e084da33ea4
marcosan <marcoxavibsc@gmail.com>
2018-01-31 13:43:10 -0500

    [NEW] popup

--- mainpage/static/js/main.js
+++ mainpage/static/js/main.js
@@ -65,21 +65,21 @@ jQuery(document).ready(function($) {
         });
 
 
-        $(window).on("scroll", function() {
-            if($(window).scrollTop() > 100) {
-                $(".header").addClass("active");
-            } else {
-                //remove the background property so it comes transparent again (defined in your css)
-               $(".header").removeClass("active");
-            }
-        });
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 100) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".header").removeClass("active");
+        }
+    });
 
 
-	/************** Mixitup (Filter Projects) *********************/
-    	$('.projects-holder').mixitup({
-            effects: ['fade','grayscale'],
-            easing: 'snap',
-            transitionSpeed: 400
-        });
+/************** Mixitup (Filter Projects) *********************/
+	$('.projects-holder').mixitup({
+        effects: ['fade','grayscale'],
+        easing: 'snap',
+        transitionSpeed: 400
+    });
 
 });
